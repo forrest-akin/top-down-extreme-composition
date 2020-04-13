000d7e395a50b896ea5713f82e4dc10cbab2d43c
fakin <fakin@cj.com>
2020-04-12 19:19:56 -0700

    enable support for different slide types

--- presentation/Presentation.jsx
+++ presentation/Presentation.jsx
@@ -1,32 +1,37 @@
 import  React from 'react'
-import  { Appear , Box , CodePane , CodeSpan , Deck
-        , FlexBox , FullScreen , Grid , Heading , Image
-        , ListItem , Markdown , Notes , OrderedList , Progress
-        , Slide , SpectacleLogo , Stepper , Text , UnorderedList
-        , indentNormalizer } from 'spectacle'
+import  { CodePane , Deck , Heading , Slide , Stepper } from 'spectacle'
 
 
 const Presentation =
     ( { theme , template , slides } ) => (
         <Deck theme={ theme } template={ template } transitionEffect="fade">
-            { slides.map(
-                ( { heading , code , steps } , index ) => (
-                    <Slide backgroundImage="linear-gradient(to bottom, #2a2139 75%, #34294f)" transitionEffect="slide" key={ heading + index }>
-                        <Heading fontSize="h3"> { heading } </Heading>
-                        <Stepper defaultValue={ [] } values={ steps }>
-                            { ( { autoFillHeight = true , start , end , size = 18 } ) => (
-                                <CodePane
-                                    autoFillHeight
-                                    fontFamily={ 'Dank Mono' }
-                                    fontSize={ size }
-                                    highlightStart={ start }
-                                    highlightEnd={ end }
-                                    language={ 'typescript' }
-                                >
-                                    { code }
-                                </CodePane> ) }
-                        </Stepper>
-                    </Slide> ) ) } </Deck> )
+            { slides.map( renderSlide ) }
+        </Deck> )
 
+const renderSlide =
+    ( { type , props } , index ) =>
+        slideRenderersByType[ type ]( props , index )
+
+const renderCodeSlide =
+    ( { heading , code , steps } , index ) => (
+        <Slide backgroundImage="linear-gradient(to bottom, #2a2139 75%, #34294f)" transitionEffect="slide" key={ heading + index }>
+            <Heading fontSize="h3"> { heading } </Heading>
+            <Stepper defaultValue={ [] } values={ steps }>
+                { ( { autoFillHeight = true , start , end , size = 18 } ) => (
+                    <CodePane
+                        autoFillHeight
+                        fontFamily={ 'Dank Mono' }
+                        fontSize={ size }
+                        highlightStart={ start }
+                        highlightEnd={ end }
+                        language={ 'typescript' }
+                    >
+                        { code }
+                    </CodePane> ) }
+            </Stepper>
+        </Slide> )
+
+const slideRenderersByType =
+    { code : renderCodeSlide }
 
 export  { Presentation }
