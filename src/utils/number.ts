aab7da69685923a7bc4e7e3772a6fa8378e3b937
fakin <fakin@cj.com>
2020-04-13 21:58:00 -0700

    consistent style

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,31 +1,36 @@
-import  { juxt
+import  { juxt , pipe
         , Binary , Curried2 , CurriedEndo2 , Endo2 } from './function'
 
 
 const add : Endo2< number > =
-    ( x , y ) => x + y
+    ( x , y ) =>
+        x + y
 
 const div : Endo2< number > =
-    ( x , y ) => x / y
+    ( x , y ) =>
+        x / y
 
 const { floor } = Math
 
 const intdiv : Endo2< number > =
-    ( x , y ) =>
-        floor( div( x , y ) )
+    pipe( div
+        , floor )
 
 const lt : Curried2< number , number , boolean > =
-    x => y => y < x
+    x => y =>
+        y < x
 
 const modulo : Endo2< number > =
-    ( x , y ) => x % y
+    ( x , y ) =>
+        x % y
 
 const divmod : Binary< number , number , Tuple2< number > > =
     juxt( intdiv
         , modulo ) as Binary< number , number , Tuple2< number > >
 
 const mult : CurriedEndo2< number > =
-    x => y => x * y
+    x => y =>
+        x * y
 
 
 export  { add
