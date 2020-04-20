fac68bcaa39a1164142b2749254f40678e2e46ce
fakin <fakin@cj.com>
2020-04-20 13:30:31 -0700

    refactor with `throwError`

--- generateCodeForSlides.js
+++ generateCodeForSlides.js
@@ -6,17 +6,22 @@ const walkDir =
     ( dir
     , prefix = '' ) =>
         fs.readdir( dir
-            , ( err , files ) => {
-                if ( err ) throw err
-                files.forEach( file => inspectFile( dir , prefix , file ) ) } )
+            , ( err , files ) =>
+                err ? throwError( err )
+                : inspectFiles( dir , prefix ,files ) )
+
+const inspectFiles = 
+    ( dir , prefix , files ) =>
+        files.forEach(
+            file => inspectFile( dir , prefix , file ) )
 
 const inspectFile =
     ( dir , prefix , file
     , filepath = path.join( dir , file ) ) =>
         fs.stat( filepath
-            , ( err , stats ) => {
-                if ( err ) throw err
-                handleFile( stats , filepath , prefix , file ) } )
+            , ( err , stats ) =>
+                err ? throwError( err )
+                : handleFile( stats , filepath , prefix , file ) )
 
 const handleFile =
     ( stats , filepath , prefix , file ) =>
@@ -28,19 +33,22 @@ const copyFile =
     ( filepath , prefix , file ) =>
         fs.readFile( filepath
             , 'utf8'
-            , ( err , fileText ) => {
-                if ( err ) throw err
-                writeFile( prefix , file , flieText ) } )
+            , ( err , fileText ) =>
+                err ? throwError( err )
+                : writeFile( prefix , file , flieText ) )
 
 const writeFile =
     ( prefix , file , fileText
     , [ exportName ] = file.split( '.' )
-    , fileName = `presentation/code/${ prefix }${ file }`) =>
+    , fileName = `presentation/code/${ prefix }${ file }` ) =>
         fs.writeFile( path.resolve( __dirname , fileName )
             , `export const ${ exportName } = \`${ fileText }\``
-            , ( err , result ) => {
-                if ( err ) throw err
-                console.log( `> ${ fileName }` ) } )
+            , ( err , result ) =>
+                err ? throwError( err )
+                : console.log( `> ${ fileName }` ) )
+
+const throwError =
+    ( error ) => { throw error }
 
 
 walkDir( path.resolve( __dirname , 'src' ) )
